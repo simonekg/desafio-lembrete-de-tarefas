c13aac887b35d8e681eec394985d10874f2c89d4
SimoneKG <simonekoniski@gmail.com>
2021-07-12 19:03:12 -0300

    Finalizado

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,13 +3,14 @@ import { View, Text, StyleSheet, Image } from 'react-native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 import logoImg from '../assets/images/logo/logo.png';
+import { TasksList } from './TasksList';
 
 interface HeaderProps {
   tasksCounter: number;
 }
 
 export function Header({ tasksCounter }: HeaderProps) {
-  // const tasksCounterText = TODO render 'tarefa' if tasksCounter equals 1, otherwise render 'tarefas'
+  const tasksCounterText =  tasksCounter === 1 ? 'tarefa' : 'tarefas';
   
   return (
     <View style={styles.container}>
@@ -17,7 +18,7 @@ export function Header({ tasksCounter }: HeaderProps) {
       
       <View style={styles.tasks}>
         <Text style={styles.tasksCounter}>Você tem </Text>
-        {/* <Text style={styles.tasksCounterBold}>{tasksCounter} {tasksCounterText}</Text> */}
+        {<Text style={styles.tasksCounterBold}>{tasksCounter} {tasksCounterText}</Text>}
       </View>
     </View>
   )
